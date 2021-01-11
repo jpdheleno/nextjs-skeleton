c9628b7190a75346ab5ae3138aaf5d31f1884b0b
João Heleno <jpdheleno@gmail.com>
2021-01-11 22:41:34 +0000

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Head from 'next/head'
+import * as s from '../styles/pages/Home'
+// inline
+import NextLogo from '../assets/next.svg'
+
+const Home: React.FC = () => {
+  return (
+    <s.Container>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+
+      <NextLogo />
+      <h1>ReactJS Structure</h1>
+      <p>A ReactJS + Next.js structure</p>
+    </s.Container>
+  )
+}
+
+export default Home
